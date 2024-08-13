6826ffc610b152509ade21656ada09a8c833858a
Jorge Sevilla <solopunks803@gmail.com>
2024-08-12 23:33:45 -0300

    Creação do repository

--- src/main/java/com/todo/list/entity/Todo.java
+++ src/main/java/com/todo/list/entity/Todo.java
@@ -6,9 +6,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "todos")
+@Data
 public class Todo {
 
 	@Id
