da288eb3f60041c13c7a66a67329afbd3ab692c7
Jorge Sevilla <solopunks803@gmail.com>
2024-09-10 06:31:19 -0300

    Implementação do service

--- src/main/java/com/todo/list/service/Todoservice.java
+++ src/main/java/com/todo/list/service/Todoservice.java
@@ -3,9 +3,11 @@ package com.todo.list.service;
 import java.util.List;
 
 import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.todo.list.entity.Todo;
+import com.todo.list.exception.BadRequestException;
 import com.todo.list.repository.TodoRepository;
 
 @Service
@@ -23,19 +25,28 @@ public class Todoservice {
 	}
 
 	public List<Todo> list() {
-		Sort sort = Sort.by("prioridade").and(Sort.by("nome"));
+		Sort sort = Sort.by(Direction.DESC, "prioridade").and(Sort.by(Direction.ASC, "id"));
 
 		return repository.findAll(sort);
 	}
 
-	public List<Todo> update(Todo todo) {
-		repository.save(todo);
+	public List<Todo> update(Long id, Todo todo) {
+		repository.findById(id).ifPresentOrElse((existingTodo) -> {
+			todo.setId(id);
+			repository.save(todo);
+		}, () -> {
+			throw new BadRequestException("Todo %d não existe! ".formatted(id));
+		});
+
 		return list();
 
 	}
 
 	public List<Todo> delete(Long id) {
-		repository.deleteById(id);
+		repository.findById(id).ifPresentOrElse((existingTodo) -> repository.delete(existingTodo), () -> {
+			throw new BadRequestException("Todo %d não existe! ".formatted(id));
+		});
+
 		return list();
 	}
 
